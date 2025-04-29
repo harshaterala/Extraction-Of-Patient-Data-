85b4e6e20cb88c7d9c793c3bf7fd691873fa20f3
T Harshavardhan <148187267+harshaterala@users.noreply.github.com>
2025-04-29 13:02:09 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from PIL import Image
+import pytesseract
+from utils import parse_lab_report_text
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+app = FastAPI()
+
+@app.post("/get-lab-tests")
+async def get_lab_tests(file: UploadFile = File(...)):
+    try:
+        image = Image.open(file.file)
+        text = pytesseract.image_to_string(image)
+
+        lab_data = parse_lab_report_text(text)
+
+        return JSONResponse(content={"is_success": True, "data": lab_data})
+    except Exception as e:
+        return JSONResponse(content={"is_success": False, "error": str(e)}, status_code=500)
